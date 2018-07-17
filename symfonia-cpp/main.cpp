111324743dcd51e203a87818b76018546e7cba64
Apotheon <apotheonx@gmail.com>
2018-07-17 16:30:52 +0200

    1

---
+++ symfonia-cpp/main.cpp
@@ -0,0 +1,6 @@
+﻿#include <iostream> //biblioteka obsługi strumieni i/p
+int main() //główna funkcja, int ponieważ zwraca exit code
+{
+	std::cout << "Witamy na pokladzie"; //std zawiera się w przestrzeni nazw std
+}
+
